16dc1b8f9bb5f0b966684a2bb8a4cf8b48e4081b
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2023-01-03 10:24:04 -0300

    feat: create summary calculations

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,39 +1,59 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { useContext } from "react";
 import { TransactionsContext } from "../../contexts/TransactionsContexts";
+import { priceFormatter } from "../../utils/formatter";
  import { SummaryCard, SummaryContainer } from "./styles";
 
  export function Summary() {
   const { transactions } = useContext(TransactionsContext)
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  );
+  
    return (
-     <SummaryContainer>
-       <SummaryCard>
-         <header>
-           <span>Entradas</span>
-           <ArrowCircleUp size={32} color="#00b37e" />
-         </header>
+      <SummaryContainer>
+        <SummaryCard>
+          <header>
+            <span>Entradas</span>
+            <ArrowCircleUp size={32} color="#00b37e" />
+          </header>
 
-         <strong>R$ 17.400,00</strong>
-       </SummaryCard>
+          <strong>{priceFormatter.format(summary.income)}</strong>
+        </SummaryCard>
 
-       <SummaryCard>
-         <header>
-           <span>Saídas</span>
-           <ArrowCircleDown size={32} color="#f75a68" />
-         </header>
+        <SummaryCard>
+          <header>
+            <span>Saídas</span>
+            <ArrowCircleDown size={32} color="#f75a68" />
+          </header>
 
-         <strong>R$ 17.400,00</strong>
-       </SummaryCard>
+          <strong>{priceFormatter.format(summary.outcome)}</strong>
+        </SummaryCard>
 
-       <SummaryCard variant="green">
-         <header>
-           <span>Total</span>
-           <CurrencyDollar size={32} color="#fff" />
-         </header>
+        <SummaryCard variant="green">
+          <header>
+            <span>Total</span>
+            <CurrencyDollar size={32} color="#fff" />
+          </header>
 
-         <strong>R$ 17.400,00</strong>
-       </SummaryCard>
-     </SummaryContainer>
+          <strong>{priceFormatter.format(summary.total)}</strong>
+        </SummaryCard>
+      </SummaryContainer>
    );
  }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContexts";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { SearchForm } from "./SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
@@ -16,31 +17,26 @@ export function Transactions() {
       <TransactionsContainer>
         <SearchForm />
 
-         <TransactionsTable>
-           <tbody>
-             <tr>
-               <td width="50%">Desenvolvimento de site</td>
-               <td>
-                 <PriceHighlight variant="income">
-                   R$ 12.000,00
-                 </PriceHighlight>
-               </td>
-               <td>Venda</td>
-               <td>13/04/2022</td>
-             </tr>
-             <tr>
-               <td width="50%">Hambúrguer</td>
-               <td>
-                 <PriceHighlight variant="outcome">
-                   -R$ 59,00
-                 </PriceHighlight>
-               </td>
-               <td>Alimentação</td>
-               <td>10/04/2022</td>
-             </tr>
-           </tbody>
-         </TransactionsTable>
-       </TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+              {transactions.map(transaction => {
+                return (
+                  <tr key={transaction.id}>
+                    <td width="50%">{transaction.description}</td>
+                    <td>
+                      <PriceHighlight variant={transaction.type}>
+                        {transaction.type === 'outcome' && '- '}
+                        {priceFormatter.format(transaction.price)}
+                      </PriceHighlight>
+                    </td>
+                    <td>{transaction.category}</td>
+                    <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
